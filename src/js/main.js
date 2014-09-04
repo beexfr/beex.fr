53569deae8b594f548f0684853e08260a7da99fd
Louis Chenais <luigi@mbp-de-luigi.beex.local>
2014-09-04 17:28:47 +0200

    Added ScrollSPy

--- src/js/main.js
+++ src/js/main.js
@@ -73,8 +73,8 @@ var cbpAnimatedHeader = (function() {
  
     var docElem = document.documentElement,
         header = $('#l-header'),
-        contact_link = $('#l-header nav a'),
-        contact_link_before = $('#l-header nav a::before'),
+        contact_link = $('#nav').find('a'),
+        contact_link_before = $('#nav a::before'),
         didScroll = false,
         changeHeaderOn = 300;
  
@@ -161,6 +161,50 @@ $('#collaborate').waypoint(function() {
   });
   $(this).waypoint('disable');
 }, { offset: '35%' });
+
+
+//ScrollSpy
+(function($){
+
+    var sections = [],
+        id = false,
+        nav = $('#nav'),
+        links = $('a', nav);
+        
+
+    //We save here all the links of our header in a table previously declared
+    links.each(function(){
+        sections.push($($(this).attr('href')));
+    });
+
+    //We now listen to the user scoll to make it match to half our viewport.
+    //Half viewport because this is the point where we choose wether or not to style our links.
+    $(window).scroll(function(e){
+        var scrollTop = $(this).scrollTop() + ($(window).height() / 2);
+        console.log(scrollTop);
+
+        //Now we have to compare the value of the scrollTop to the top distance of our links
+        for(var i in sections){
+            var section = sections[i];
+            //If the top distance of our section is higher than our scrollTop value we aim it
+            if (scrollTop > section.offset().top) {
+                scrolled_id = section.attr('id');
+            } 
+        }
+
+        //We only want to style the link if the id changes
+        if(scrolled_id !== id){
+            //We store in our id variable the scrolled_id value in order to prevent 
+            //the function to execute itself over and over even if the id didn't change.
+            id = scrolled_id;
+            //Now we style the specific element which has the id previously generated
+            links.removeClass('clicked');
+            $('a[href="#' + id + '"]', nav).addClass('clicked');  
+            console.log('Menu change pour : ' + id);
+        }
+    });
+    
+})(jQuery);
 
 //Dynamic Year in Footer
 $('#year').text(new Date().getFullYear());
